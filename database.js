1dd2acdbce62932aadfe9ce6cc512c7b1fa3f913
Juniorreis25 <juninho1981@gmail.com>
2025-04-08 10:20:52 -0300

    Add files via upload

--- database.js
+++ database.js
@@ -2,10 +2,19 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/financeiro');
-    console.log('Conectado ao banco de dados');
-  } catch (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
+    const uri = process.env.MONGO_URI; // Agora pega do ambiente Render
+    if (!uri) {
+      throw new Error('MONGO_URI não definida!');
+    }
+
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('✅ Conectado ao banco de dados');
+  } catch (error) {
+    console.error('❌ Erro ao conectar ao banco de dados:', error);
   }
 };
 
